fc9043c74ae3e3049ecd35d68db7f347d88efcd2
GBC <cooleriip2@gmail.com>
2025-03-05 00:07:03 +0300

    Воссоздание ветки 8 спринта, после бага с размеров файла

---
+++ Image Feed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+
+    @IBOutlet weak var cellDateLabel: UILabel!
+    @IBOutlet weak var cellLikeButton: UIButton!
+    @IBOutlet weak var cellPhoto: UIImageView!
+}
